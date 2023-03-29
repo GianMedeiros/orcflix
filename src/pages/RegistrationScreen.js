03bf31a346bd7fa5554920f7bae3e0b840b2d0bb
Pedro Ramos Sousa Reis <pedro.rsr0308@gmail.com>
2023-03-28 23:46:11 -0300

    Tela de registro html e css prontos

--- src/pages/RegistrationScreen.js
+++ src/pages/RegistrationScreen.js
@@ -1,7 +1,40 @@
+import styles from '../styles/RegistrationScreen.module.css'
+
 export default function RegistrationScreen() {
     return (
-        <h1>
-            Hello tela de cadastro
-        </h1>
+        <>
+            <h1>ORC'FLIX</h1>
+            <div className={styles.caixa_registro}>
+                <h2>Crie sua conta</h2>
+                <form>
+                    <div className={styles.divisoes}>
+                        <p>Nome:</p>
+                        <input></input>
+                    </div>
+                    <div className={styles.divisoes}>
+                        <p>Sobrenome:</p>
+                        <input></input>
+                    </div>
+                    <div className={styles.divisoes}>
+                        <p>Nome de usuário:</p>
+                        <input></input>
+                    </div>
+                    <div className={styles.divisoes}>
+                        <p>Senha:</p>
+                        <input></input>
+                    </div>
+                    <div className={styles.divisoes}>
+                        <p>Confirmar senha:</p>
+                        <input></input>
+                    </div>
+                    <div className={styles.botoes}>
+                        <button className={styles.botao_criar_conta}>Criar conta</button>
+                        <button className={styles.botao_voltar}>Voltar</button>
+                    </div>
+                </form>
+            </div>
+        </>
+
+
     );
 }
