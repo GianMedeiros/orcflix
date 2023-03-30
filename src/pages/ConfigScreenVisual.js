631701d24f7c54d750e90550893c3c2f67f8de2e
Ricardo <lrsj2003@gmail.com>
2023-03-30 19:39:27 -0300

    a tela alterna de modo de edicao e nao edicao

--- src/pages/ConfigScreenVisual.js
+++ src/pages/ConfigScreenVisual.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import styles from "../styles/ConfigScreenVisual.module.css"
 
 export default function ConfigScreenVisual() {
+    const [isEditing, setIsEditing] = useState(false)
     // Essa tela tem duas versoes a no modo de edicao e 
     // a no modo de visualizacao, ver prototipo no figma
     return (
@@ -8,28 +10,57 @@ export default function ConfigScreenVisual() {
             <div className={styles.titulo}>
                 <h2>Configurações</h2>
             </div>
-            <div className={styles.bloco_fundo}>
-                <div className={styles.dados}>
-                    <div className={styles.conta}>
-                        <h3>Conta:</h3>
-                        <p>Nome:</p>
-                        <div className={styles.dados_do_usuario}>Dados</div>
-                        <p>Sobrenome:</p>
-                        <div className={styles.dados_do_usuario}>Dados</div>
-                        <p>Nome de usuário:</p>
-                        <div className={styles.dados_do_usuario}>Dados</div>
-                        <p>Descrição:</p>
-                        <div className={styles.dados_do_usuario}>Dados</div>
-                        <button className={styles.botao_editar}>Editar perfil</button>
-                    </div>
-                    <div className={styles.voltar}>
-                        <button className={styles.botao_voltar}>Voltar</button>
+            {isEditing ? // Tela caso esteja editando o perfil
+                //TODO: Substituir as div por input e criar um forms
+                <div className={styles.bloco_fundo}>
+                    <div className={styles.dados}>
+                        <div className={styles.conta}>
+                            <h3>Conta:</h3>
+                            <p>Nome:</p>
+                            <div className={styles.dados_do_usuario}>Dados</div>
+                            <p>Sobrenome:</p>
+                            <div className={styles.dados_do_usuario}>Dados</div>
+                            <p>Nome de usuário:</p>
+                            <div className={styles.dados_do_usuario}>Dados</div>
+                            <p>Descrição:</p>
+                            <div className={styles.dados_do_usuario}>Dados</div>
+                            <button className={styles.botao_editar}>Salvar Alterações</button>
+                        </div>
+                        <div className={styles.voltar}>
+                            <button className={styles.botao_voltar}
+                                onClick={() => { setIsEditing(false) }}>
+                                Cancelar
+                            </button>
+                        </div>
                     </div>
                 </div>
-                <div className={styles.excluir}>
-                    <button className={styles.botao_excluir}>Exluir conta</button>
+                :  // Tela caso não esteja editando o perfil
+                <div className={styles.bloco_fundo}>
+                    <div className={styles.dados}>
+                        <div className={styles.conta}>
+                            <h3>Conta:</h3>
+                            <p>Nome:</p>
+                            <div className={styles.dados_do_usuario}>Dados</div>
+                            <p>Sobrenome:</p>
+                            <div className={styles.dados_do_usuario}>Dados</div>
+                            <p>Nome de usuário:</p>
+                            <div className={styles.dados_do_usuario}>Dados</div>
+                            <p>Descrição:</p>
+                            <div className={styles.dados_do_usuario}>Dados</div>
+                            <button className={styles.botao_editar}
+                                onClick={() => { setIsEditing(true) }}>
+                                Editar perfil
+                            </button>
+                        </div>
+                        <div className={styles.voltar}>
+                            <button className={styles.botao_voltar}>Voltar</button>
+                        </div>
+                    </div>
+                    <div className={styles.excluir}>
+                        <button className={styles.botao_excluir}>Exluir conta</button>
+                    </div>
                 </div>
-            </div>
+            }
         </>
     );
 }
