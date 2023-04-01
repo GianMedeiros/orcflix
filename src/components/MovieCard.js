c1bbf418087d8d43d12fb9e884619680679414b7
Ricardo <lrsj2003@gmail.com>
2023-03-31 23:16:26 -0300

    limpesa de comentarios para colaboradores

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,11 +4,6 @@ const imagesURL = "https://image.tmdb.org/t/p/w500" // TODO: Talvez mover para u
 
 // Recebe um objeto movie como parametro
 export default function MovieCard({ movie }) {
-    // Esse componente deve conter o card do filme aqueles menores 
-    // que levam para a tela de detalhe de filme. Alem de ter o poster do filme deve ter o seu nome, 
-    // e principalmente o icone de assistir mais tarde e tambem deve ser exibido no lugar o icone de 
-    // assitido caso o usuario logado ja tenha visto o filme, isso é um dos requisitos que os 
-    // filmes assitidos devem ser exibidos diferentemente.
     return (
         <div className={styles.card}>
             <a href={`/movie/${movie.id}`}>
--- src/components/MoviesBox.js
+++ src/components/MoviesBox.js
@@ -4,14 +4,6 @@ import styles from "../styles/MoviesBox.module.css"
 
 // Recebe um array de objetos de filmes
 export default function MoviesBox({ movies, isCarousel = false }) {
-    // Esse componente deve receber os filmes que serao listados e retornar um
-    // container com os card de filme agrupados para usar esse componente na 
-    // Tela de busca onde os cards exibidos serao os do filtro, na tela de 
-    // detalhe onde os cards exibidos serao os semelhantes e na tela do 
-    // usuario onde os cards exibidos serao os de favorito/Assitidos/Mais Tarde
-    // Tambem seria interessante usar esse elemento na tela inicial assim
-    // mas para isso é necessario um condicional para nao criar os elementos 
-    // em caixa mas sim em bandeija (tipo a da netflix)
     return (
         <div className={isCarousel ? styles.carousel : styles.movies_list}>
             {movies.length > 0 && movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
